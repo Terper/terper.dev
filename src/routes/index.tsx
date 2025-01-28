99d4c9810eac608708a1d2f3cd8d0f6dc1bcc918
Terper <jann.totterman@gmail.com>
2025-01-29 01:22:47 +0200

    fetch test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,30 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+import { GhRepos } from "../types/GhRepos";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  const query = useQuery({
+    queryKey: ["github repos"],
+    queryFn: async (): Promise<GhRepos[]> => {
+      const response = await fetch("https://api.github.com/users/Terper/repos");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return await data.sort(
+        (a: GhRepos, b: GhRepos) =>
+          new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
+      );
+    },
+  });
+
+  if (query.data) {
+    console.log(query.data);
+  }
+
+  return <div></div>;
 }
