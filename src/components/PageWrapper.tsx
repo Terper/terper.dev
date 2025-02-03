c3835f36c4b389ed2d453fa426ac34bb14a2ba0d
Terper <jann.totterman@gmail.com>
2025-02-03 02:17:12 +0200

    prepare for different styles

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -5,11 +5,7 @@ type Props = {
 };
 
 const PageWrapper = ({ children }: Props) => {
-  return (
-    <main className="min-h-[calc(100vh-60px)] max-w-5xl m-auto">
-      {children}
-    </main>
-  );
+  return <main className="min-h-[calc(100vh-60px)]">{children}</main>;
 };
 
 export default PageWrapper;
