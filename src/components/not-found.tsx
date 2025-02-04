91473839531c4f30052ed515ccf3d40508f0d96f
Terper <jann.totterman@gmail.com>
2025-02-04 08:11:38 +0200

    updated 404

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,5 +1,10 @@
 const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-dvh">
+      <div className="font-mono text-5xl">404</div>
+      <div>Page not found</div>
+    </div>
+  );
 };
 
 export default NotFound;
